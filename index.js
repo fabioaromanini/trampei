0c82c33cffcc7d104f8beaa1d4647124485c55f7
Fabio Romanini <fabioaromanini@gmail.com>
2018-06-18 07:57:27 -0300

    gets access token from google login

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./credentials');
+
 const trampeiApp = express();
 
-trampeiApp.get('/', (req, res) => {
-  res.send('HI');
-});
+const googleLoginKeys = {
+  clientID: keys.clientID,
+  clientSecret: keys.clientSecret,
+  callbackURL: '/auth/google/callback'
+};
+
+passport.use(
+  new GoogleStrategy(googleLoginKeys, accessToken => console.log(accessToken))
+);
 
 const PORT = process.env.PORT || 5000;
 trampeiApp.listen(PORT);
