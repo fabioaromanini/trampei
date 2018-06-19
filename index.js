346ed399885c4bd7b74e6096c1dc3d7543243b37
Fabio Romanini <fabioaromanini@gmail.com>
2018-06-18 21:51:41 -0300

    finishes cookie based session

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const mongoCredentials = require('./credentials/dev').mongo;
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
+const credentials = require('./credentials/dev');
+
+const mongoCredentials = credentials.mongo;
+const cookieKey = credentials.cookieKey;
 
 require('./models/User');
 require('./services/passport');
 
 mongoose.connect(mongoCredentials.uri);
+
 const trampeiApp = express();
+
+trampeiApp.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 1000,
+    keys: [cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(trampeiApp);
 
