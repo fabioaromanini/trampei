e1164ff85d2401643cbdee192b91c4a8e21be99e
Fabio Romanini <fabioaromanini@gmail.com>
2018-06-24 02:04:04 -0300

    adds configuration for remote build

--- index.js
+++ index.js
@@ -27,5 +27,14 @@ trampeiApp.use(passport.session());
 
 require('./routes/authRoutes')(trampeiApp);
 
+if (process.env.ENVIRONMENT === 'dev-server') {
+  trampeiApp.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 trampeiApp.listen(PORT);
