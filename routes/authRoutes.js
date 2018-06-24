8d857e7345d658cc3ed4d92f49c72dd7847f9a08
Fabio Romanini <fabioaromanini@gmail.com>
2018-06-24 00:23:05 -0300

    fixes redirect on google login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,13 @@ module.exports = app => {
     })
   );
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/trampos');
+    }
+  );
 
   app.get('/auth/logout', (req, res) => {
     req.logout();
