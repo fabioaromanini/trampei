f94273b19bdf70d182bd200c6745cb3ca5c7f5b8
Fabio Romanini <fabioaromanini@gmail.com>
2018-07-04 00:45:57 -0300

    adds greetings for authenticated users

--- pinoquio-client/src/components/NavBar/NavBar.jsx
+++ pinoquio-client/src/components/NavBar/NavBar.jsx
@@ -5,6 +5,12 @@ import { connect } from 'react-redux';
 import './NavBar.css';
 
 class NavBar extends Component {
+  greetings() {
+    if (this.props.user) {
+      return <span id="greetings">Olá, {this.props.user.name}</span>;
+    }
+  }
+
   loginOrLogout() {
     if (this.props.user) {
       return <a href="/auth/logout">Logout</a>;
@@ -26,6 +32,7 @@ class NavBar extends Component {
           <Link to="/">#trampei</Link>
         </div>
         <div className="NavBar_menu">
+          {this.greetings()}
           <Link to="/contato">Contato</Link>
           <Link to="/quemsomos">Quem somos</Link>
           {this.trampos()}
